3b5021aa32a1a643996f4fdb00a1c7341f4b25f9
tumbuhanman <77344115+tumbuhanman@users.noreply.github.com>
2021-01-21 21:20:51 +0700

    Data Dom Note Clemens Ferryanto

---
+++ LearnDOM/doom1.js
@@ -0,0 +1,14 @@
+var jmlAngkot= 10;
+var angkotBeroperasi = 6;
+var noAngkot = 1; 
+while(noAngkot <= angkotBeroperasi){
+    console.log('Angkot No.'  + noAngkot + ' beroprasi dengan baik.');
+
+noAngkot++;
+
+}
+ for( noAngkot = 7; noAngkot <= jmlAngkot; noAngkot++){
+ console.log('Angkot No.'  + noAngkot + ' tidak beroprasi dengan baik.');
+
+ }
+
