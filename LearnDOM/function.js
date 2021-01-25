29b0a4529e98f2d3a26ad8ece1758e59ebf19825
Faisal Juliandra <faisaljuliandra@gmail.com>
2021-01-25 11:17:46 +0700

    Update Learn

---
+++ LearnDOM/function.js
@@ -0,0 +1,11 @@
+function lessThan100(a, b) {
+    if (a + b < 100) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(lessThan100(22, 15));
+console.log(lessThan100(83, 34));
+console.log(lessThan100(3, 77));
