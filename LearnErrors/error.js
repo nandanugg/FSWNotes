cd082e3338370461c9a0efd3ad9bf05bed5fe9cf
giovmahe <giovannimaendra@gmail.com>
2021-01-26 23:46:48 +0700

    Add note

---
+++ LearnErrors/error.js
@@ -0,0 +1,24 @@
+// deal with error
+const user = {
+    name: Giova,
+    age: 21,
+    Address: "Karangjati"
+}
+
+function sum(first, second) {
+    return first + second;
+}
+
+setTimeout(() => {
+    console.log("This is from setTimeout()");
+}, 100)
+
+//syntax error, terkait dengan tulisan
+
+//type error, salah memasukkan parameter dengan suatu fungsi atau variabel
+// parameter dimasukkan tipe data yang salah, seperti parameter minta function,
+// tapi dimasukkan malah variable, atau minta variable number malah diisi string.
+
+//Reference error, suatu variable tidak mereferensi apapun
+// Kedepan reference erro juga bakal sering terjadi jika berhubungan dengan
+// data dari internet, karena data dari internet kadang bergantung pada koneksi
