9309c56eea37a353afadde77a7e867797460d4b5
Abiyyu Ramadhan <edseljobs@gmail.com>
2021-02-04 23:44:56 +0700

    Add LearnErrors

---
+++ LearnErrors/script.js
@@ -0,0 +1,12 @@
+const obj = {
+    name: "L Lawliet",
+    age: "18",
+    Address: "Tokyo Street"
+}
+
+function sumof(st, nd) {
+    return st + nd
+}
+// setTimeout(() => {
+//     console.log("the message is up to me"));
+// }, 100);
